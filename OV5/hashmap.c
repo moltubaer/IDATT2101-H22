2479e37ec7ce4c787fa16e04dfee9d166998cc9a
Alexander Moltu <alexamol@stud.ntnu.no>
2022-10-12 17:24:59 +0200

    Ferdig med øving 6

--- OV5/hashmap.c
+++ OV5/hashmap.c
@@ -5,18 +5,20 @@
 #include <string.h>
 
 #define SIZE 10000000
-
 int arr[SIZE];
-int singleCollisions;
-int doubleCollisions;
+//long singleCollisions;
+//long doubleCollisions;
+long Collisions;
 struct DataItem {
         int data;
         int key;
 };
 int mod;
-int hashTable[12000000]; //størrelse på hashtable kan justeres basert på hva som ønskes av lastfaktor mot performance
-int test = 1;
+int hashTable[12000017]; //Tabellstørrelse endret til primtall 
+			  
+int prime = 5;
 int htSize = sizeof(hashTable) / sizeof(hashTable[0]);
+
 bool isprime(int p){
         int k;
         for(k=2; k <= p / 2; ++k){
@@ -24,44 +26,65 @@ bool isprime(int p){
                        return false;
         }
         return true;
+
 }
+
+bool isEven(int e){
+	if(e%2 == 0){
+		return true;
+		}
+	else{
+		return false;
+	}
+}
+
 int hash1(int h1){
         return h1%(htSize);
 }
+
 int hash2(int h1){
         return (2*h1+1)%(htSize);
+}
+int primeHash(int p){
+	return prime-(p%prime);
 }
 int probe(int index1, int index2,int x){
         return (index1 + index2*x)%(htSize);
 }
+
+
 int insert(int num){
+	//Endret fra forrige innlevering
         int h1 = hash1(num);
         if(hashTable[h1] == 0){
                 hashTable[h1] = num;
-                singleCollisions++;
         }
         else{
-                int h2 = hash2(num);
+                int h2 = primeHash(num);
                 int n = probe(h1,h2,1);
+
+
                 while(true){
-                        n = (mod+(h1+h2)%htSize);
+			// (h1+h1)%htSize endret til (n+h2)%htSize så det ikke blir lineær probing men
+			// at dobbel hashing fungerer korrekt 
+                        n = ((n+h2)%htSize);
+
                         if(hashTable[n]==0){
                                 hashTable[n]=num;
-				mod = 0;
                                 break;
                         }
-                        mod++;
-                        doubleCollisions++;
+  	
+                        Collisions++;
+
                 }
         }
 }
 
 int main()
 {
-        int i;       	
-	int range = SIZE*2;
+        int i;       
         for(i=0;i<SIZE;i++){
-                arr[i] = rand()%range;		
+                arr[i] = rand()%htSize;
         }
 	clock_t t;
 	t = clock();
@@ -72,11 +95,9 @@ int main()
 	double time_taken = ((double)t)/CLOCKS_PER_SEC;
 	float lastfaktor;
         lastfaktor = SIZE / (float)htSize;
-        printf("Single collisions: %d\n", singleCollisions);
-        printf("Double collisions: %d\n", doubleCollisions);
-	printf("Total collisions: %d\n", singleCollisions+doubleCollisions);	
+     //   printf("Single collisions: %li\n", singleCollisions);
+     //   printf("Double collisions: %li\n", doubleCollisions);
+	printf("Total collisions: %li\n", Collisions);	
 	printf("Lastfaktor: %f\n", lastfaktor);
 	printf("Excecution time: %f seconds\n", time_taken);
-
-	//Har snakket med helge angående kravet om sammenligning med ferdiglaget hashmap men siden dette ikke er tilgjengelig i C vurderte vi at dette kravet kunne droppes
 }
